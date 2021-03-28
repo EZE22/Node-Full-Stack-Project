d5a731e36030833dc722697eee0c700ba874b9d8
adam <everett.ingram22@gmail.com>
2021-03-28 11:54:46 -0400

    Refactored for just the udagram project

--- image-filter-starter-code/src/server.ts
+++ image-filter-starter-code/src/server.ts
@@ -1,20 +1,14 @@
 import express, {Request, Response} from 'express';
-import { sequelize } from './sequelize';
-import { IndexRouter } from './controllers/v0/index.router';
 import bodyParser from 'body-parser';
-import { V0MODELS } from './controllers/v0/model.index';
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
 (async () => {
-
-  await sequelize.addModels(V0MODELS);
-  await sequelize.sync();
 
   // Init the Express application
   const app = express();
 
   // Set the network port
-  const port = process.env.PORT || 8080;
+  const port = process.env.PORT || 8082;
   
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
@@ -49,16 +43,6 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   });
   /**************************************************************************** */
   //! END @TODO1
-
-  //CORS Should be restricted
-  app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:8100");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    next();
-  });
-
-  app.use('/api/v0/', IndexRouter)
-
   
   // Root Endpoint
   // Displays a simple message to the user
