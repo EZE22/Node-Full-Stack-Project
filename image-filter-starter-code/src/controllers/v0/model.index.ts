605299aad976da0dc35c082b6688e8cb2f8d10df
adam <everett.ingram22@gmail.com>
2021-03-28 10:48:43 -0400

    Set up Sequelize

---
+++ image-filter-starter-code/src/controllers/v0/model.index.ts
@@ -0,0 +1,6 @@
+// @ts-ignore
+import { FeedItem } from '../../../../../../cloud_developer/cloud-developer/course-02/exercises/udacity-c2-restapi/src/controllers/v0/feed/models/FeedItem';
+// @ts-ignore
+import { User } from './users/models/User';
+
+export const V0MODELS = [ FeedItem, User ];
